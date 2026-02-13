30754ed5adbd342fc8d0904f5bbb35fd0a086204
karltokoekambi <acamer.guillaume@gmail.com>
2026-02-13 18:19:22 +0100

    wine list added

--- src/app/components/wine-list/wine-list.component.ts
+++ src/app/components/wine-list/wine-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WineService } from '../../services/wine.service';
+import { WineBottle } from '../../models/wine.model';
 
 @Component({
   selector: 'app-wine-list',
-  templateUrl: './wine-list.component.html',
-  styleUrl: './wine-list.component.sass'
+  templateUrl: './wine-list.component.html'
 })
-export class WineListComponent {
-  wines = [];
+export class WineListComponent implements OnInit {
+  wines: WineBottle[] = [];
+
+  constructor(private wineService: WineService) { }
+
+  ngOnInit(): void {
+    this.wineService.getBottles().subscribe(data => {
+      this.wines = data;
+    });
+  }
 }
